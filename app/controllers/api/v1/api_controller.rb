31a48097d79eb04d0f2ba6dd4965d7a7603d2b05
Ty Kujawa <comsenzce13@gmail.com>
2013-10-21 18:44:30 -0600

    gemfile updated + modules added Application Controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,3 @@
 class Api::V1::ApiController < ApplicationController
-	include ActionController::MimeResponds
-	include ActionController::Helpers
-	include ActionController::Cookies
-	include ActionController::ImplicitRender
+
 end
