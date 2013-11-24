0b2f6cccdb815b2f1e971e13f212d4c39be38d6b
Alex Krautmann <alex.krautmann@gmail.com>
2013-11-24 00:17:30 -0600

    Added Friend Search: Tighter Looks

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ActiveRecord::Base
   #   if no arg passed, returns all users
   def self.search(search, current_user)
     if search
-      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+      find(:all, :conditions => ['user_name LIKE ?', "%#{search}%"])
     else
       find(:all, :conditions => (current_user ? ["id != ?", current_user.id] : []))
     end
