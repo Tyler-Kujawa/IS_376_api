eb82bccd1e984e0ca28d7d83d95f97aba999d07e
Alex Krautmann <alex.krautmann@gmail.com>
2013-11-25 02:22:55 -0600

    Fixed graph

--- app/assets/javascripts/Modules/GraphBodyHandler.js
+++ app/assets/javascripts/Modules/GraphBodyHandler.js
@@ -2,7 +2,6 @@ var GraphBodyHandler = function (json, el) {
     // Useful Vars
     var $graphDiv = el.find('.user-graph');
     var KEYS = {PENDING: 0, SENT: 1, ONGOING: 2, SUBMITTED: 3, FULFILLED: 4, APPROVED: 5, DECLINED: 6};
-
     // Event handlers
     el.find('.fulfilledCommit').click(function () {
         clearGraph();
--- app/assets/javascripts/Modules/UserGraph.js
+++ app/assets/javascripts/Modules/UserGraph.js
@@ -1,9 +1,10 @@
 var UserGraph = function(json, el, cStatus, title){
     var c = json.user.commitments;
+    console.log(c);
     var monthArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     for (var i=0; i<c.length; i++) {
         // IF: commitment was fulfilled
-        var deadline = new Date(c[i].deadline);
+        var deadline = new Date(c[i].commitment.deadline);
         if (c[i].status = cStatus) {
             var monthDue = deadline.getMonth();
             monthArr[monthDue]++;
